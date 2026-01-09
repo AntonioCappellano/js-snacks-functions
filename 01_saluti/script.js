9d867777ffb6827ec486e6cd0d70e3a371b5f966
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-09 13:32:13 +0100

    function first snack

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,15 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
-
+const userName = "Mario";
 
 // Dichiara la funzione qui.
-
+function greet(name) {
+  console.log(`ciao ${name}`);
+  return `Ciao ${name}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+greet("Mario");
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
